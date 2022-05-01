e00828f81b33dbffe81b74a8590d1b8399be1fad
Ajinkya Kulkarni <95011676+ajinkyahk@users.noreply.github.com>
2022-05-01 15:49:22 +0530

    Update emails_test.py

--- week 5/Qwiklabs Assessment: Implementing Unit Testing/emails_test.py
+++ week 5/Qwiklabs Assessment: Implementing Unit Testing/emails_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import unittest
 from emails import find_email
-class TestFile(unittest.TestCase):
+class EmailsTest(unittest.TestCase):
   def test_basic(self):
     testcase = [None, "Bree", "Campbell"]
     expected = "breee@abc.edu"
@@ -9,6 +9,10 @@ class TestFile(unittest.TestCase):
   def test_one_name(self):
     testcase = [None, "John"]
     expected = "Missing parameters"
+    self.assertEqual(find_email(testcase), expected)
+  def test_two_name(self):
+    testcase = [None, "Roy","Cooper"]
+    expected = "No email address found"
     self.assertEqual(find_email(testcase), expected)
 if __name__ == '__main__':
   unittest.main()
